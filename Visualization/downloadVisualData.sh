fe7c8cf53b22130542c753bd8b8ce42a70f917da
Abduallah Mohamed <abduallah.adel.omar@gmail.com>
2022-03-06 15:33:15 -0800

    Update downloadVisualData.sh

--- Visualization/downloadVisualData.sh
+++ Visualization/downloadVisualData.sh
@@ -1,3 +1,3 @@
-wget -O vis.zip 'https://utexas.box.com/shared/static/m7mduw6uyngx52wdf4j23ggv3sorslnw.zip'
+wget -O vis.zip 'https://utexas.box.com/shared/static/0cy1yp7mmcxza5x34lzb0d9lcupnnn2t.zip'
 unzip -q vis.zip
 rm -rf vis.zip
